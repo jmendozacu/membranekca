6f7131ea6ed11f69392059c5d403d9bde2f6721a
unknown <vishal.141414@gmail.com>
2017-07-20 11:50:53 +0530

    Magento Extension For Remove Compare Dt : 20-7-2017

---
+++ app/code/community/Wfs/DisableCompare/Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * MageryThemes
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://magery-themes.com/MAGERY-LICENSE.txt
+ *
+ *
+ * MAGENTO EDITION USAGE NOTICE
+ *
+ * This package designed for Magento COMMUNITY edition
+ * MageryThemes does not guarantee correct work of this extension
+ * on any other Magento edition except Magento COMMUNITY edition.
+ * MageryThemes does not provide extension support in case of
+ * incorrect edition usage.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future.
+ *
+ * @category   Wfs
+ * @package    Wfs_DisableCompare
+ * @copyright  Copyright (c) 2013 MageryThemes (http://magery-themes.com)
+ * @license    http://magery-themes.com/MAGERY-LICENSE.txt
+ */
+class Wfs_DisableCompare_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
