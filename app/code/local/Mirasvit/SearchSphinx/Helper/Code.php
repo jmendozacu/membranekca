e35e52c736ec8e9a578e983bc3d5ca4912edcb33
unknown <vishal.141414@gmail.com>
2017-07-20 12:22:16 +0530

    Mirasvit Autocomplete Search Extension Dt: 20-7-2017

---
+++ app/code/local/Mirasvit/SearchSphinx/Helper/Code.php
@@ -0,0 +1,10 @@
+<?php
+class Mirasvit_SearchSphinx_Helper_Code extends Mirasvit_MstCore_Helper_Code
+{
+    protected $k = "YQEJVM61DN";
+    protected $s = "SSU";
+    protected $l = "12331";
+    protected $v = "2.3.2";
+    protected $b = "1219";
+    protected $d = "itechdeals.com";
+}
